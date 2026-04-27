151b0b6bf9d5fc256c9d99d1180671aa11b04416
Stars-XK <Stars-XK@users.noreply.github.com>
2026-04-27 08:47:10 +0000

    feat: add WardrobeUIController to dynamically build clothing selection UI

---
+++ Assets/Scripts/UI/WardrobeUIController.cs
@@ -0,0 +1,101 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DesktopPet.DressUp;
+
+namespace DesktopPet.UI
+{
+    public class WardrobeUIController : MonoBehaviour
+    {
+        [Header("System References")]
+        public WardrobeManager wardrobeManager;
+        public DressUpManager dressUpManager;
+
+        [Header("UI References")]
+        public GameObject wardrobePanel;
+        public Transform contentContainer;
+        public GameObject clothingButtonPrefab;
+        
+        [Header("Category Tabs")]
+        public Button tabHair;
+        public Button tabTop;
+        public Button tabBottom;
+        public Button tabShoes;
+
+        private void Start()
+        {
+            if (wardrobeManager != null)
+            {
+                wardrobeManager.OnWardrobeLoaded += InitializeUI;
+            }
+
+            // Setup tab listeners
+            if (tabHair != null) tabHair.onClick.AddListener(() => ShowCategory(ClothingType.Hair));
+            if (tabTop != null) tabTop.onClick.AddListener(() => ShowCategory(ClothingType.Top));
+            if (tabBottom != null) tabBottom.onClick.AddListener(() => ShowCategory(ClothingType.Bottom));
+            if (tabShoes != null) tabShoes.onClick.AddListener(() => ShowCategory(ClothingType.Shoes));
+        }
+
+        private void OnDestroy()
+        {
+            if (wardrobeManager != null)
+            {
+                wardrobeManager.OnWardrobeLoaded -= InitializeUI;
+            }
+        }
+
+        private void InitializeUI()
+        {
+            Debug.Log("[WardrobeUI] Wardrobe loaded, initializing UI...");
+            // Show Top category by default
+            ShowCategory(ClothingType.Top);
+        }
+
+        public void ShowCategory(ClothingType category)
+        {
+            // Clear existing buttons
+            foreach (Transform child in contentContainer)
+            {
+                Destroy(child.gameObject);
+            }
+
+            if (!wardrobeManager.AvailableClothes.TryGetValue(category, out List<ClothingPart> parts))
+            {
+                Debug.Log($"[WardrobeUI] No clothing found for category: {category}");
+                return;
+            }
+
+            foreach (ClothingPart part in parts)
+            {
+                GameObject btnObj = Instantiate(clothingButtonPrefab, contentContainer);
+                
+                // Try to find a text component to set the name
+                Text btnText = btnObj.GetComponentInChildren<Text>();
+                if (btnText != null)
+                {
+                    btnText.text = part.partName;
+                }
+
+                Button btn = btnObj.GetComponent<Button>();
+                if (btn != null)
+                {
+                    btn.onClick.AddListener(() => 
+                    {
+                        Debug.Log($"[WardrobeUI] Equipping {part.partName}");
+                        dressUpManager.EquipPart(part.gameObject);
+                        
+                        // TODO: Update SaveManager with the new equipped ID
+                    });
+                }
+            }
+        }
+
+        public void ToggleWardrobePanel()
+        {
+            if (wardrobePanel != null)
+            {
+                wardrobePanel.SetActive(!wardrobePanel.activeSelf);
+            }
+        }
+    }
+}
